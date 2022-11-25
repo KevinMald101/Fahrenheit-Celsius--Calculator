9222a3880ab42d6c06a9d305921af32686e6883d
Kevin M <93846113+KevinMald101@users.noreply.github.com>
2022-11-24 22:04:27 -0500

    Upd8 (Fixed Calculator, Freezing & boiling points)
    1. Fixed the calculator not checking your input to be just a character. (f, c)
    
    2. Added boiling and freezing points for Fahrenheit. When you reach those levels, it'll print out a message. (Celsius soon to be made but not rn since i can't)

--- Python/TempCalc.py
+++ Python/TempCalc.py
@@ -1,9 +1,14 @@
 def CCalculator(Fahrenheit):
-    Celsius = (int(Fahrenheit) - 32) * .5556
+    Celsius = (int(Fahrenheit) - 32) /1.8
     print(str(Celsius) + "°C")
 
 def FCalculator(Celsius):
     Fahrenheit = (int(Celsius) * 1.8) + 32
+    if Fahrenheit <= freezingPointF:
+        print("Freezing point reached")
+    if Fahrenheit >= boilingPointF:
+        print("Boiling point reached")
+    
     print(str(Fahrenheit) + "°F")
 
 def inputAmount():
@@ -15,23 +20,25 @@ def inputAmount():
         Amount = input()
     return Amount
     
+freezingPointC = 0
+freezingPointF = 32
+boilingPointF = 212
 isInputWrong = True
 while isInputWrong:
-    print("What would you like to calculate: Fahrenheit or Celsius")
-    Type = input().lower()
-    Inputs = ["fahrenheit", "f", "celsius", "c"]
+    #print("What unit would you like to use: Fahrenheit or Celsius")
+    Type = input("What unit would you like to use: Fahrenheit or Celsius: ").lower()
     isInputWrong = False
 
-    if Type == "fahrenheit" or "f":
+    if Type == 'fahrenheit' or Type == 'f':
         print("Amount you want to make Celsius:")
         Amount = inputAmount()
         CCalculator(Amount)
-    elif Type == "celsius" or "c":
+    elif Type == 'celsius' or Type == 'c':
         print("Amount you want to make Fahrenheit:")
         Amount = inputAmount()
         FCalculator(Amount)
     else:
         isInputWrong = True
-        print("Your input of {} was not a valid input type".format(Type))
+        print("Your input of \" {} \" was not a valid input type".format(Type))
 
 
